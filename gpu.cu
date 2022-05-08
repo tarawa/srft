a235780ed9fc81679c8e8786102d9ae5aef78f05
Jiaqi Yang <yangjq17@gmail.com>
2022-05-08 21:03:53 +0800

    workaround

--- gpu.cu
+++ gpu.cu
@@ -1,6 +1,12 @@
 #include "common.h"
 #include <cuda.h>
 
+void init(int N, int d, int n_ranks, const int *f, const int *perm, const int *r, Transform transform) {
+}
+
+void init_nlogd(int N, int d, int n_ranks, const int *f, const int *perm, const int *r, Transform transform) {
+}
+
 void srft(int N, int d, int n_ranks, const int *f, const int *perm, const double *a, double *sa_re, double *sa_im, const int *r, Transform transform) {
 }
 
